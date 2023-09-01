79571f7035e086521fefad3dff155cb19640d49e
Ketrin <leanworld2020@gmail.com>
2023-09-01 14:23:35 +0300

    pointer-events

--- src/components/Buttons/ButtonsAddProductToCard/ButtonsAddProductToCard.tsx
+++ src/components/Buttons/ButtonsAddProductToCard/ButtonsAddProductToCard.tsx
@@ -16,7 +16,7 @@ const ButtonsAddProductToCard = () => {
         В корзину
       </button>
 
-      <CSSTransition in={openModal} timeout={2000} classNames="modal-product-add" unmountOnExit onExited={() => setOpenModal(false)} >
+      <CSSTransition in={openModal} timeout={2000} classNames="modal-product-add" unmountOnExit onExited={() => setOpenModal(false)}>
         <ModalProductAddition closeModal={setOpenModal} />
       </CSSTransition>
     </>
