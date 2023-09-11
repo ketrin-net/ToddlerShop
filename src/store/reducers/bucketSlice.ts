53d0bc925a0ed47329a393f7f291f6ba650f75cf
Ketrin <leanworld2020@gmail.com>
2023-09-11 19:31:15 +0300

    fix naming

--- src/store/reducers/bucketSlice.ts
+++ src/store/reducers/bucketSlice.ts
@@ -1,18 +1,17 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { Product } from '../../models/product';
 import { RootState } from '../store';
-import { enableMapSet } from 'immer';
 
-enableMapSet();
+type ProductInBucket = Array<Product & { count: number }>;
 
 const item = localStorage.getItem('products') !== null ? JSON.parse(localStorage.getItem('products') as string) : [];
 
-const setItemFunc = (item: Array<Product & { count: number }>) => {
+const saveProductsInStorage = (item: ProductInBucket) => {
   localStorage.setItem('products', JSON.stringify(item));
 };
 
 export interface Bucket {
-  products: Array<Product & { count: number }>;
+  products: ProductInBucket;
 }
 
 const initialState: Bucket = {
@@ -30,7 +29,7 @@ export const bucketSlice = createSlice({
         state.products.push({ ...action.payload, count: 1 });
       }
 
-      setItemFunc(state.products);
+      saveProductsInStorage(state.products);
     },
     changeCountProduct: (state, action: PayloadAction<{ id: number; addOrDelete: boolean }>) => {
       const prod = state.products.find((item) => item.id === action.payload.id);
@@ -39,15 +38,15 @@ export const bucketSlice = createSlice({
         prod.count = action.payload.addOrDelete ? prod.count + 1 : prod.count > 1 ? prod.count - 1 : prod.count;
       }
 
-      setItemFunc(state.products);
+      saveProductsInStorage(state.products);
     },
     deleteProductInBucket: (state, action: PayloadAction<{ id: number }>) => {
-      let prod = state.products.find(item => item.id === action.payload.id);
-      const indexProd = state.products.findIndex(item => item === prod);
+      const prod = state.products.find((item) => item.id === action.payload.id);
+      const indexProd = state.products.findIndex((item) => item === prod);
 
       state.products.splice(indexProd, 1);
 
-      setItemFunc(state.products);
+      saveProductsInStorage(state.products);
     },
   },
 });
