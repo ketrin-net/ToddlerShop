d3494f0fb894eb73e6270adadaa978351e8cbf35
Ketrin <leanworld2020@gmail.com>
2023-10-01 00:51:45 +0300

    mobile catalog

--- src/pages/CatalogPage/ListSubCategory/ListSubCategory.tsx
+++ src/pages/CatalogPage/ListSubCategory/ListSubCategory.tsx
@@ -3,25 +3,29 @@ import { Path } from '../../../enums/Path';
 import { selectCategoriesInState } from '../slice/catalogSlice';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ListSubCategory = () => {
   const navigate = useNavigate();
-  const { categoryId } = useParams();
+  const { categoryId, subCategoryId } = useParams();
   const categories = useSelector(selectCategoriesInState);
   const allSubCategories = categories?.find((i) => i.id === parseInt(categoryId as string))?.subCategory;
 
   const changePage = (id: number) => () => {
-    const path = Path.SubCategoryPage.replace(':categoryId', categoryId as string).replace(':subCategoryId', id.toString());
-    navigate(path, { replace: true});
+    const path = `/category/${categoryId}/${id.toString()}`;
+    navigate(path);
   };
 
   return (
-    <div className="list-subcategory">
+    <div id="list-subcategory">
       {allSubCategories?.map((item) => (
-        <button key={item.id} onClick={changePage(item.id)}>
-          {item.name}
-        </button>
+        <>
+          <div className="subcategory">
+            <button key={item.id} onClick={changePage(item.id)}>
+              {item.name}
+            </button>
+          </div>
+        </>
       ))}
     </div>
   );
